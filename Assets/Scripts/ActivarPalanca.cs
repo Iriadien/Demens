d0e145c2f3797c81756e561514a6d9512e3106e0
Iriadien <42410087+Iriadien@users.noreply.github.com>
2020-02-01 20:47:03 +0100

    cosas
    This reverts commit c463616e2fae52260974c2a7f3bc41757aba513b.

---
+++ Assets/Scripts/ActivarPalanca.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarPalanca : MonoBehaviour
+{
+    public Animator animator;
+    public float duration;
+
+    private bool cooldown;
+    private float time;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !cooldown)
+        {
+            //Debug.Log("uses");
+            animator.SetBool("Use", true);
+            cooldown = true;
+            
+            //StartCoroutine(Wait(duration));
+        }
+
+        if (cooldown)
+        {
+            time += Time.deltaTime;Debug.Log(time);
+            if (time >= duration)
+            {
+                cooldown = false;
+                animator.SetBool("Use", false);
+                time = 0;
+            }
+        }
+    }
+
+    IEnumerator Wait (float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        animator.SetBool("Use", false);
+        cooldown = false;
+    }
+}
---
+++ Assets/Scripts/Empujar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Empujar : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Pusheable") animator.SetBool("Push", true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Pusheable") animator.SetBool("Push", false);
+    }
+}
