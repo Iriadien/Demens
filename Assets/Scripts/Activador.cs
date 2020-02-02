a9174bedda1cd425d9d3af78dc0e6379acff0aed
Iriadien <42410087+Iriadien@users.noreply.github.com>
2020-02-02 10:16:12 +0100

    activadores

---
+++ Assets/Scripts/Activador.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activador : MonoBehaviour
+{
+    public GameObject objetoActivador;
+
+    public List<GameObject> objetosParaActivar;
+
+    private EstadoObjeto eo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eo = objetoActivador.GetComponent<EstadoObjeto>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!eo.Activado) return;
+        Debug.Log("cosas");
+
+    }
+}
---
+++ Assets/Scripts/EstadoObjeto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstadoObjeto : MonoBehaviour
+{
+    public Activador activador;
+    public ActivadorAscensor activadorAscensor;
+    public bool Activado;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("colli");
+        if (activador != null && activador.gameObject == collision.gameObject)
+        {
+            Debug.Log("activar");
+            Activado = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (activador != null && activador.gameObject == collision.gameObject)
+        {
+            Activado = false;
+        }
+    }
+}
